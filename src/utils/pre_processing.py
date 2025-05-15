98f7c7ee40351e6340fb1d1a057bdadd3b3f4b0e
Dageus <101069446+Dageus@users.noreply.github.com>
2025-05-15 02:54:59 +0100

    feat(project): changed to TypeScript Vite project

--- src/utils/pre_processing.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-DATASET_PATH = 'src/assets/data/complete_dataset.csv'
-
-df = pd.read_csv(DATASET_PATH)
-
-# Save processed data for React
-df.to_json('src/assets/data/f1_processed.json', orient='records')
